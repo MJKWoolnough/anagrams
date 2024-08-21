d753708ed73120034d5a7eb851228dabe8bcac60
Michael Woolnough <michael.woolnough@gmail.com>
2024-08-21 20:05:04 +0100

    reformatted

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ Loop:
 		if len(args) == 0 {
 			return errors.New("no words")
 		}
+
 		switch args[0] {
 		case "-p":
 			incomplete = true
@@ -36,11 +37,14 @@ Loop:
 			break Loop
 		}
 	}
+
 	bs, err := os.ReadFile("/usr/share/dict/words")
 	if err != nil {
 		return fmt.Errorf("error reading dictionary: %w", err)
 	}
+
 	ss := strings.Split(strings.ToLower(byteSliceToString(bs)), "\n")
+
 	if plural {
 		for _, word := range ss {
 			if !strings.HasSuffix(word, "s") {
@@ -48,35 +52,50 @@ Loop:
 			}
 		}
 	}
+
 	sort.Strings(ss)
+
 	wordsList := make([]map[byte][]string, len(args))
+
 	for n, words := range args {
 		wordsList[n] = make(map[byte][]string, strings.Count(words, " ")+1)
+
 		for _, word := range strings.Split(words, " ") {
 			var first byte
+
 			if len(word) > 0 {
 				first = strings.ToLower(word[:1])[0]
 			}
+
 			wordsList[n][first] = append(wordsList[n][first], word)
+
 			sort.Strings(wordsList[n][first])
 		}
 	}
+
 	buildAnagrams(ss, wordsList, make([]int, 0, len(wordsList)), make([]byte, 0, len(wordsList)), make([][]string, 0, len(wordsList)), incomplete)
 	sort.Sort(res)
+
 	var last string
+
 	for _, result := range res {
 		if result.Word != last {
 			fmt.Println(result.Word)
+
 			last = result.Word
 		}
+
 		for _, m := range result.Makeup {
 			fmt.Print("->")
+
 			for _, w := range m {
 				fmt.Print(" ", w)
 			}
+
 			fmt.Println()
 		}
 	}
+
 	return nil
 }
 
@@ -89,7 +108,9 @@ func buildAnagrams(ss []string, wordsList []map[byte][]string, done []int, sofar
 					continue Loop
 				}
 			}
+
 			d := append(done, n)
+
 			for b, word := range wordsL {
 				if b == 0 {
 					buildAnagrams(ss, wordsList, d, sofar, words, incomplete)
@@ -99,11 +120,14 @@ func buildAnagrams(ss []string, wordsList []map[byte][]string, done []int, sofar
 			}
 		}
 	}
+
 	if (incomplete || len(done) == len(wordsList)) && len(sofar) > 0 {
 		s := byteSliceToString(sofar)
+
 		if pos := sort.SearchStrings(ss, s); pos < len(ss) && ss[pos] == s {
-			currWords = nil
 			printWords(words, make([]int, 0, len(words)))
+
+			currWords = nil
 			res = append(res, result{
 				Word:   string(sofar),
 				Makeup: currWords,
@@ -138,12 +162,16 @@ var currWords [][]string
 func printWords(w [][]string, pos []int) {
 	if len(pos) == len(w) {
 		words := make([]string, len(pos))
+
 		for n, p := range pos {
 			words[n] = w[n][p]
 		}
+
 		currWords = append(currWords, words)
+
 		return
 	}
+
 	for n := range w[len(pos)] {
 		printWords(w, append(pos, n))
 	}
